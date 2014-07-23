b07aa4081ea98c8c81df3229ed8dbf5d450e40eb
sirkut <ntfybs@gmail.com>
2014-07-23 09:41:10 -0400

    0.18.1
    Removed IRScale and incorporated it into the main project.

--- InfernalRobotics/InfernalRobotics/Properties/AssemblyInfo.cs
+++ InfernalRobotics/InfernalRobotics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IR_TweakScale")]
+[assembly: AssemblyTitle("InfernalRobotics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IR_TweakScale")]
+[assembly: AssemblyProduct("InfernalRobotics")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("544f09f7-8e23-48af-ae28-0b191629e5bf")]
+[assembly: Guid("730bed02-4be4-4b0e-a83c-dcd1f230a498")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.0.0")]
-[assembly: AssemblyFileVersion("0.16.0.0")]
+[assembly: AssemblyVersion("0.18.1.0")]
+[assembly: AssemblyFileVersion("0.18.1.0")]

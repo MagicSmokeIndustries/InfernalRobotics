6ec95853e3efc6305b4e5c22e8d6baa51a33eea0
Chris Woerz <erendrake@gmail.com>
2015-03-29 00:37:26 -0600

    one type per file

--- InfernalRobotics/InfernalRobotics/Control/IServoGroup.cs
+++ InfernalRobotics/InfernalRobotics/Control/IServoGroup.cs
@@ -1,31 +1,8 @@
-﻿using InfernalRobotics.Module;
-
-namespace InfernalRobotics.Control
+﻿namespace InfernalRobotics.Control
 {
     public interface IServoGroup    
     {
         string Name { get; set; }
         float ElectricChargeRequired { get; }
-    }
-
-    internal class ServoGroup : IServoGroup
-    {
-        private readonly MuMechToggle rawServo;
-
-        public ServoGroup(MuMechToggle rawServo)
-        {
-            this.rawServo = rawServo;
-        }
-
-        public string Name
-        {
-            get { return rawServo.groupName; }
-            set { rawServo.groupName = value; }
-        }
-
-        public float ElectricChargeRequired
-        {
-            get { return rawServo.GroupElectricChargeRequired; }
-        }
     }
 }
---
+++ InfernalRobotics/InfernalRobotics/Control/ServoGroup.cs
@@ -0,0 +1,25 @@
+﻿using InfernalRobotics.Module;
+
+namespace InfernalRobotics.Control
+{
+    internal class ServoGroup : IServoGroup
+    {
+        private readonly MuMechToggle rawServo;
+
+        public ServoGroup(MuMechToggle rawServo)
+        {
+            this.rawServo = rawServo;
+        }
+
+        public string Name
+        {
+            get { return rawServo.groupName; }
+            set { rawServo.groupName = value; }
+        }
+
+        public float ElectricChargeRequired
+        {
+            get { return rawServo.GroupElectricChargeRequired; }
+        }
+    }
+}

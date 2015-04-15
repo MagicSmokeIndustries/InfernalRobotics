04bfbe58557ef7d471c875e078568218206943ec
ZiwKerman <stepan.andreev@gmail.com>
2015-04-15 20:21:47 +0100

    Bunch of comments on Interfaces

--- InfernalRobotics/InfernalRobotics/Control/IServo.cs
+++ InfernalRobotics/InfernalRobotics/Control/IServo.cs
@@ -4,19 +4,43 @@ namespace InfernalRobotics.Control
 {
     public interface IPart
     {
+        /// <summary>
+        /// Servo's name
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Settable only, Highlight the servo part on the vessel
+        /// </summary>
         bool Highlight { set; }
+        /// <summary>
+        /// Amount of EC consumed by the servo
+        /// </summary>
         float ElectricChargeRequired { get; set; }
     }
 
     public interface IServo : IPart
     {
+        /// <summary>
+        /// Implementation of servo's mechanical components
+        /// </summary>
         IMechanism Mechanism { get; }
+        /// <summary>
+        /// Implementation of presets
+        /// </summary>
         IPresetable Preset { get; }
+        /// <summary>
+        /// Servo's Group related implementation
+        /// </summary>
         IControlGroup Group { get; }
+        /// <summary>
+        /// Implementation of servo's keyboard control elements
+        /// </summary>
         IServoInput Input { get; }
 
-        // Scheduled for execution
+        /// <summary>
+        /// Soon to be deprecated reference to the actual servo
+        /// </summary>
         MuMechToggle RawServo { get; }
     }
 }

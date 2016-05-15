f4f655325223796c396a5bed415fe0824cde5826
Stepan Andreev <stepan.andreev@gmail.com>
2016-05-15 11:26:46 +0100

    More auto-strut fixes

--- InfernalRobotics/InfernalRobotics/Command/ServoController.cs
+++ InfernalRobotics/InfernalRobotics/Command/ServoController.cs
@@ -348,13 +348,16 @@ namespace InfernalRobotics.Command
             var activeVesselWheels = v.FindPartModulesImplementing<ModuleWheelBase>();
             foreach(var mwb in activeVesselWheels)
             {
-                mwb.autoStrut = value;
                 if (value)
                 {
-                    mwb.CycleWheelStrut();
+                    if(!mwb.autoStrut) //we only need to Cycle once
+                        mwb.CycleWheelStrut();
                 }
                 else
                     mwb.ReleaseWheelStrut();
+
+                mwb.autoStrut = value;
+
             }
         }
 
